d54d6500bae9c1dc491a71280d2c047243a462b3
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-13 23:37:12 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,4 @@
 class Song < ApplicationRecord
-  validates :title, presence: true uniqueness: true
+  validates :title, presence: true, uniqueness: true
   validates :release_year, presence: true if: released
 end
