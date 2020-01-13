f56e766e35aa0be0d067dd2956f393ca58e86cc0
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-13 23:30:20 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,3 @@
 Rails.application.routes.draw do
-  get 'songs/new'
-
-  get 'songs/create'
-
-  get 'songs/edit'
-
-  get 'songs/update'
-
-  get 'songs/destroy'
-
-  get 'songs/index'
-
-  get 'songs/show'
-
 resource :songs
 end
