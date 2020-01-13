cbada3541247783eb1c7e8012219cbed826b0128
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-13 23:43:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,4 @@
 class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: true
-  validates :released_year, numericality: {less_than_or_equal_to: Time.new.year}
+  validates :release_year, numericality: {less_than_or_equal_to: Time.new.year}
 end
