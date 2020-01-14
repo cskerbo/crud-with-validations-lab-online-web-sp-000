49b0f269a3c27604f7203ddc652c351789745db0
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-14 00:10:32 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,15 @@ class SongsController < ApplicationController
   end
 
   def create
+    @song = Song.new(song_params)
+ 
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      # re-render the :new template WITHOUT throwing away the invalid @song
+      render :new
+    end
   end
 
   def edit
