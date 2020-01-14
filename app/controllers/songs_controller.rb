5677d40104be09130ac882be9f17cbc4c896252b
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-14 00:21:10 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,7 +29,7 @@ class SongsController < ApplicationController
 
   def destroy
     Song.find(params[:id]).destroy
-    redirect_to song_path(@song)
+    redirect_to songs_url
   end
 
   def index
